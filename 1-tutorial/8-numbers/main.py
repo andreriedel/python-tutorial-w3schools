f8105baa58e16adafda1ff9762201ff0ec706d70
André Riedel <andreriedelz@gmail.com>
2024-04-07 23:56:16 -0300

    feat(tutorial): add numbers

---
+++ 1-tutorial/8-numbers/main.py
@@ -0,0 +1,14 @@
+import random as rd
+
+x = 1 # int
+y = 2.8 # float
+z = 3 + 5j # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 10.5e100 # float
+print(x)
+
+print(rd.randrange(1, 10))
