9bf2f73aa66ca2958432e3a983865784d2de7dde
André Riedel <andreriedelz@gmail.com>
2024-05-15 17:49:20 -0300

    feat(tutorial): add math

---
+++ 1-tutorial/30-math/main.py
@@ -0,0 +1,28 @@
+import math
+
+values = [5, 10, 25]
+
+x = min(values)
+y = max(values)
+
+print(x)
+print(y)
+
+# ---------------------------------------------------------------------------- #
+
+print(abs(-7.25))
+
+print(pow(4, 3))
+
+# ---------------------------------------------------------------------------- #
+
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+print(math.pi)
