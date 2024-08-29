4b7ff9d2e691efb166e28372ef7886e28a7328d3
André Riedel <andreriedelz@gmail.com>
2024-08-29 16:26:29 -0300

    feat(tutorial): add scope

---
+++ 1-tutorial/27-scope/main.py
@@ -0,0 +1,23 @@
+x = 300
+
+def myfunc():
+    global x
+    x = 200
+
+myfunc()
+print(x)
+
+# ---------------------------------------------------------------------------- #
+
+# The nonlocal keyword is used to work with variables inside nested functions.
+# The nonlocal keyword makes the variable belong to the outer function.
+
+def myfunc1():
+    x = "Jane"
+    def myfunc2():
+        nonlocal x
+        x = "hello"
+    myfunc2()
+    return x
+
+print(myfunc1())
