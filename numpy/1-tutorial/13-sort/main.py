a201935e7d3f08fb634539abd250b8a031014bc8
André Riedel <andreriedelz@gmail.com>
2024-11-02 19:12:37 -0300

    feat(numpy tutorial): add sort

---
+++ numpy/1-tutorial/13-sort/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+# ---------------------------- sorting a 2-D array --------------------------- #
+
+arr = np.array([[2, 3, 4], [5, 0, 1]])
+print(np.sort(arr))
