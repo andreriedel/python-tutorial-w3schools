dd1e1a4badfbf94a8fda859e44e4bc7d650efa56
André Riedel <andreriedelz@gmail.com>
2025-03-24 14:07:17 -0300

    feat(pandas cleaning): add cleaning wrong data

---
+++ pandas/2-cleaning-data/3-cleaning-wrong-data/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df.loc[7, 'Duration'] = 45
+
+print(df.to_string())
+
+# ---------------------------------------------------------------------------- #
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.loc[x, "Duration"] = 120
+        
+# ------------------------------- removing rows ------------------------------ #
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.drop(x, inplace=True)
