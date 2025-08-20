bfed0a030bbcf03eb75e42e6b07451271dd45c28
André Riedel <andreriedelz@gmail.com>
2025-08-20 17:37:27 -0300

    feat(scipy tutorial): add interpolation

---
+++ scipy/8-interpolation/main.py
@@ -0,0 +1,37 @@
+from scipy.interpolate import interp1d
+from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import Rbf
+import numpy as np
+
+# ----------------------------- 1d interpolation ----------------------------- #
+
+xs = np.arange(10)
+ys = 2*xs + 1
+
+interp_func = interp1d(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
+
+# --------------------------- spline interpolation --------------------------- #
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = UnivariateSpline(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
+
+# -------------------- radial basis function interpolation ------------------- #
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = Rbf(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
